bd37375fbf122563810110b12d0b5810b5403201
Liza Belonina <fkme.fuhua@gmail.com>
2024-04-28 14:19:18 +0400

    Ладно, я сделала треугольник

--- Figure.cpp
+++ Figure.cpp
@@ -1,29 +1,16 @@
 ﻿#include <iostream>
 using namespace std;
 int main() {
-	// Просто программа выводящая прямоугольник
 	setlocale(LC_ALL, "RUS");
-	int height, width;
 	cout << "Введите высоту: ";
-	cin >> height;
-	cout << "Введите ширину: ";
-	cin >> width;
-	for (int i = 1; i <= height; i++) {
-		if (i == 1 || i == height) {
-			for (int j = 1; j <= width; j++) {
-				cout << '*';
-			}
+	int n;
+	cin >> n;
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= n - i; j++) {
+			cout << " ";
 		}
-		else {
-			for (int j = 1; j <= width; j++) {
-				if (j == 1 || j == width) {
-					cout << '*';
-				}
-				else
-				{
-					cout << " ";
-				}
-			}
+		for (int k = 1; k <= 2 * i - 1; k++) {
+			cout << '*';
 		}
 		cout << '\n';
 	}
