5eeeeaf3ad91545780b0794433d139acb0ce3dd6
Liza Belonina <sukebeyoel@gmail.com>
2024-04-28 13:29:21 +0400

    Если необходимо сделать другую фигуру, то скажите

---
+++ Figure.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "RUS");
+	int height, width;
+	cout << "Введите высоту: ";
+	cin >> height;
+	cout << "Введите ширину: ";
+	cin >> width;
+	for (int i = 1; i <= height; i++) {
+		if (i == 1 || i == height) {
+			for (int j = 1; j <= width; j++) {
+				cout << '*';
+			}
+		}
+		else {
+			for (int j = 1; j <= width; j++) {
+				if (j == 1 || j == width) {
+					cout << '*';
+				}
+				else
+				{
+					cout << " ";
+				}
+			}
+		}
+		cout << '\n';
+	}
+}
